fb937d336022a5aa9af3217e443e1f68134a3915
Requinard <d.diks@mail.blastedsolutions.com>
2015-03-20 15:47:54 +0100

    Last comments on functions in DBManager

--- ICT4EVENT/DBManager.cs
+++ ICT4EVENT/DBManager.cs
@@ -56,6 +56,9 @@ namespace ICT4EVENT
             return true;
         }
 
+        /// <summary>
+        /// Destructs DBManager
+        /// </summary>
         ~DBManager()
         {
             Dispose(true);
