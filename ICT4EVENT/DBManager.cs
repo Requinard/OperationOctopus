68f4ddf94151a0c630dc5a51fb82cdadf517d7f4
Requinard <d.diks@mail.blastedsolutions.com>
2015-03-26 14:32:09 +0100

    Added logging messages to DBManager

--- ICT4EVENT/DBManager.cs
+++ ICT4EVENT/DBManager.cs
@@ -35,17 +35,23 @@ namespace ICT4EVENT
 
             try
             {
+                Logger.Info("Attempting connection to Database.");
                 oracleConnection.Open();
+                Logger.Success("Database connection succesfully opened.");
             }
             catch (OracleException)
             {
+                Logger.Error("Database connection could not be opened.");
                 if (
                     MessageBox.Show(
                         "There seems to be an error connecting to the Database. Would you like to remove the existing database configuration?",
                         "Database error", MessageBoxButtons.YesNo) == DialogResult.Yes)
                 {
+                    Logger.Warning("Deleting database config name");
                     File.Delete(Settings.DBCONFIGFILENAME);
                 }
+                Logger.Info("Exiting application due to broken database config");
+                Logger.Destruct();
                 Environment.Exit(1);
                 return;
             }
@@ -62,6 +68,7 @@ namespace ICT4EVENT
 
         public void Dispose()
         {
+            Logger.Debug("Disposing of DBManager");
             Dispose(true);
         }
 
@@ -132,10 +139,14 @@ namespace ICT4EVENT
 
             try
             {
+                Logger.Info("Querying Database");
+                Logger.Debug("Query: " + query);
                 queryResult = oracleCommand.ExecuteReader();
+                Logger.Success("Query Successfully executed");
             }
             catch (OracleException exception)
             {
+                Logger.Error("Error querying datbase: " + exception.ToString());
                 Console.WriteLine(exception.ToString());
 
                 return null;
