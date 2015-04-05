5ae3368d9e2b3ed44e35ed1dba6db067f10a51db
luudvkeulen <luudvkeulen@gmail.com>
2015-04-05 13:17:13 +0200

    Added FTP Manager

---
+++ ICT4EVENT/FTPManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace ICT4EVENT
+{
+    abstract class FTPManager
+    {
+        public void UploadFile(string path)
+        {
+
+        }
+    }
+}
