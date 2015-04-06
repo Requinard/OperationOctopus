6cfa1b64564946e69eb8fcc9721bd3e348abd9e9
luudvkeulen <luudvkeulen@gmail.com>
2015-04-06 13:04:14 +0200

    - Class made static. - Addes 2 strings.

--- ICT4EVENT/FTPManager.cs
+++ ICT4EVENT/FTPManager.cs
@@ -8,8 +8,11 @@ using System.IO;
 
 namespace ICT4EVENT
 {
-    abstract class FTPManager
+    static abstract class FTPManager
     {
+        string FolderLocation = "ftp://88.159.165.90/Disk/Public/Media/";
+        string FileName = "sample.mp4";
+
         public void UploadFile(string path)
         {
 
