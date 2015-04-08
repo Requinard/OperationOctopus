896df2d0b53d440a1ec43589308fce9b4ce400be
Requinard <d.diks@mail.blastedsolutions.com>
2015-04-08 15:31:07 +0200

    Added initializer
    Added authentification methods
    
    Added find methods
    
    Added comments

--- ICT4EVENT/SocialMediaEventManager.cs
+++ ICT4EVENT/SocialMediaEventManager.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel;
+using System.Drawing.Text;
 using System.IO;
+using System.Linq;
 using System.Security.Cryptography;
 
 namespace ICT4EVENT
@@ -8,7 +10,23 @@ namespace ICT4EVENT
     public static class UserManager
     {
         private static RNGCryptoServiceProvider crypto;
+        private static List<UserModel> users;
 
+        public static void Initialize()
+        {
+            const string select_query = "SELECT * FROM USERS";
+
+            //TODO: query db for all users
+
+            //TODO: for all users we add the reservations
+        }
+
+        /// <summary>
+        /// Creates a new user
+        /// </summary>
+        /// <param name="username">Desired username</param>
+        /// <param name="password">Desired password</param>
+        /// <returns>New user that was created</returns>
         public static UserModel CreateUser(string username, string password)
         {
             UserModel user = new UserModel(null);
@@ -19,6 +37,51 @@ namespace ICT4EVENT
             user.Create();
 
             return user;
+        }
+
+        /// <summary>
+        /// Authenticates a user.
+        /// </summary>
+        /// <param name="username">Username of user</param>
+        /// <param name="password">Password that the user inputted</param>
+        /// <returns>Success of the operation</returns>
+        public static bool AuthenticateUser(string username, string password)
+        {
+            UserModel user = FindUser(username);
+
+            return AuthenticateUser(user, password);
+        }
+
+        /// <summary>
+        /// Authenticates a user.
+        /// </summary>
+        /// <param name="user">UserModel of the user to be authenticated</param>
+        /// <param name="password">Password that the user inputted</param>
+        /// <returns>Success of the operation</returns>
+        public static bool AuthenticateUser(UserModel user, string password)
+        {
+            if (user.Password == (Settings.salt + password))
+                return true;
+
+            return false;
+        }
+
+        public static UserModel FindUser(string username)
+        {
+            IEnumerable<UserModel> s = from user in users
+                where user.Username == username
+                select user;
+
+            return s.ToList()[0] ?? null;
+        }
+
+        public static UserModel FindUser(int id)
+        {
+            var s = from user in users
+                where user.Id == id
+                select user;
+
+            return s.ToList()[0] ?? null;
         }
     }
 }
