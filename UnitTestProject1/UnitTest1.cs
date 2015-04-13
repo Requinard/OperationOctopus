266eb7eb7c1f9869ad92a8ae92c64ec72f853be0
Requinard <d.diks@mail.blastedsolutions.com>
2015-04-13 13:18:14 +0200

    Added unit testing project

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ICT4EVENT;
+
+namespace ICT4EVENTUnitTest 
+{
+    [TestClass]
+    public class UserManagerTest 
+    {
+        [TestMethod]
+        public void CreateNewUserFromUser()
+        {
+            UserModel user = new UserModel();
+
+            user.Level = 1;
+            user.Username = "TestUser";
+            user.Password = "test";
+
+            user.RfiDnumber = "9e02130312";
+            user.Telephonenumber = "561551";
+
+            Assert.IsTrue(user.Create());
+        }
+
+        public void ReadUserFromDatabase
+    }
+}
