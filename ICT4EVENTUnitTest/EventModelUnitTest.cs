52eebc9d9b4f6ad6b1f936f3b529170663ffa86d
samynv <d.diks@mail.blastedsolutions.com>
2015-04-13 21:56:36 +0200

    Added event model testing

---
+++ ICT4EVENTUnitTest/EventModelUnitTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ICT4EVENT;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ICT4EVENTUnitTest
+{
+    [TestClass]
+    public class EventModelUnitTEst
+    {
+        [TestMethod]
+        public void CreateTest()
+        {
+            Init.Initialize();
+
+            EventModel event_item = new EventModel();
+
+            event_item.Location = "Eindhoven";
+            event_item.Description = "We're testing our things";
+            event_item.Name = "ICT Testing";
+            event_item.StartDate = DateTime.Now;
+            event_item.EndDate = DateTime.Now;
+
+            Assert.IsTrue(event_item.Create());
+        }
+    }
+}
