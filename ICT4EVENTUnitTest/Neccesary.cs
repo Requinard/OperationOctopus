7e17be4d42277e054f58c7498baf359fbea5d276
samynv <d.diks@mail.blastedsolutions.com>
2015-04-13 21:14:47 +0200

    Made autoconfig db

--- ICT4EVENTUnitTest/Neccesary.cs
+++ ICT4EVENTUnitTest/Neccesary.cs
@@ -7,6 +7,16 @@ namespace ICT4EVENTUnitTest
     {
         public static void Initialize()
         {
+            DBConfig config = new DBConfig();
+
+            config.user = "PTS08";
+            config.database = "xe";
+            config.pw = "PTS08";
+            config.host = "proftaak.me";
+            config.port = "1521";
+
+            Settings.DbConfig = config;
+
             DBManager.Initalize();
         }
     }
