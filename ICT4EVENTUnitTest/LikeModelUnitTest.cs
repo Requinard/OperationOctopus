1deff5e46acb1735dce8908cfecd72e55670d6be
leonchen7498 <leonchen@live.nl>
2015-04-14 13:59:20 +0200

    like model unit test werkt goed

--- ICT4EVENTUnitTest/LikeModelUnitTest.cs
+++ ICT4EVENTUnitTest/LikeModelUnitTest.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel.Design;
 using ICT4EVENT;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Oracle.DataAccess.Client;
 
 namespace ICT4EVENTUnitTest
 {
@@ -14,24 +15,61 @@ namespace ICT4EVENTUnitTest
             Init.Initialize();
             LikeModel like = new LikeModel();
             PostModel post = new PostModel();
-            post.Id = 112;
+            post = Init.GetExternalPost();
             post.Read();
 
             like.User = Init.getExternalTestUser();
             like.Post = post;
             
-            Assert.IsTrue(like.Create(), "Kan like niet naar database schrijven.");
+            Assert.IsTrue(like.Create(), "Can't write to database");
         }
 
         [TestMethod]
         public void ReadLike()
         {
             Init.Initialize();
-            LikeModel like = new LikeModel();
-            like.Id = 1;
+            UserModel user = Init.getExternalTestUser();
+            PostModel post = Init.GetExternalPost();
 
-            Assert.IsTrue(like.Read(), "CANT READ LIKE");
+
+            LikeModel like = new LikeModel();
+
+            string query = string.Format("SELECT Ident FROM likes WHERE UserID = '{0}' AND PostID = '{1}'", user.Id, post.Id);
+
+            OracleDataReader reader = DBManager.QueryDB(query);
+
+            Assert.IsNotNull(reader, "Could not read Like.Ident from the database.");
+
+            reader.Read();
+
+            like.Id = Int32.Parse(reader["Ident"].ToString()); 
+
+
+            Assert.IsTrue(like.Read(), "Couldn't read from database.");
         }
 
+        [TestMethod]
+        public void DestroyLike()
+        {
+            Init.Initialize();
+            UserModel user = Init.getExternalTestUser();
+            PostModel post = Init.GetExternalPost();
+
+
+            LikeModel like = new LikeModel();
+
+            string query = string.Format("SELECT Ident FROM likes WHERE UserID = '{0}' AND PostID = '{1}'", user.Id, post.Id);
+
+            OracleDataReader reader = DBManager.QueryDB(query);
+
+            Assert.IsNotNull(reader, "Could not read Like.Ident from the database.");
+
+            reader.Read();
+
+            like.Id = Int32.Parse(reader["Ident"].ToString());
+
+
+            Assert.IsTrue(like.Destroy(), "Couldn't read from database.");
+        }
     }
 }
