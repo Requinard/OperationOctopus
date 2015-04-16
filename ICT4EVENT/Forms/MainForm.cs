9185bf5dcabdd41954a09cd1d43a4ae3970f87b3
Requinard <d.diks@mail.blastedsolutions.com>
2015-04-16 11:47:20 +0200

    Added Materials

--- ICT4EVENT/Forms/MainForm.cs
+++ ICT4EVENT/Forms/MainForm.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
 using System.Windows.Forms.VisualStyles;
-using ICT4EVENT.Controls;
+;
 
 namespace ICT4EVENT
 {
