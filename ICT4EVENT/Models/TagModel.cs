9a9b956088983e039d3196acb9453a0eb090987c
Requinard <d.diks@mail.blastedsolutions.com>
2015-04-17 10:24:01 +0200

    Created tag model

--- ICT4EVENT/Models/TagModel.cs
+++ ICT4EVENT/Models/TagModel.cs
@@ -6,26 +6,65 @@ using System.Threading.Tasks;
 
 namespace ICT4EVENT.Models
 {
+    using Microsoft.SqlServer.Server;
+
+    using Oracle.DataAccess.Client;
+
     class TagModel : DBModel, IDataModelUpdate
     {
+        private string name;
+
+        public void ReadFromReader(OracleDataReader reader)
+        {
+            this.Id = Int32.Parse(reader["ident"].ToString());
+            this.name = reader["name"].ToString();
+        }
         public bool Create()
         {
-            throw new NotImplementedException();
+            string query = string.Format("INSERT INTO TAG (name) VALUES ('{0}')", Id);
+
+            if (DBManager.QueryDB(query) == null) return false;
+            return true;
         }
 
         public bool Read()
         {
-            throw new NotImplementedException();
+            string query = string.Format(READSTRING, "Tag", Id);
+
+            OracleDataReader reader = DBManager.QueryDB(query);
+
+            if (reader == null) return false;
+
+            reader.Read();
+
+            ReadFromReader(reader);
+
+            return true;
         }
 
         public bool Update()
         {
-            throw new NotImplementedException();
+            string query = string.Format(UPDATESTRING, "Tag", Id);
+
+            OracleDataReader reader = DBManager.QueryDB(query);
+
+            if (reader == null) return false;
+
+            reader.Read();
+
+            ReadFromReader(reader);
+
+            return true;
         }
 
         public bool Destroy()
         {
-            throw new NotImplementedException();
+            string query = String.Format(DESTROYSTRING, "Tag", Id);
+
+            OracleDataReader reader = DBManager.QueryDB(query);
+
+            if (reader == null) return false;
+            return true;
         }
     }
 }
