c5988444eeb81aed429eb15f4047cb224c705b4c
luudvKeulen <l.vankeulen@student.fontys.nl>
2015-04-19 14:27:36 +0200

    Fixed rfid.close()

--- ICT4EVENT/Forms/LoginForm.cs
+++ ICT4EVENT/Forms/LoginForm.cs
@@ -14,7 +14,6 @@ namespace ICT4EVENT
         {
             InitializeComponent();
 
-
             OpenRFIDConnection();
         }
 
@@ -30,8 +29,6 @@ namespace ICT4EVENT
 
         private void RFID_Tag(object sender, TagEventArgs e)
         {
-            rfid.Antenna = false;
-            rfid.LED = false;
             txtRFID.Text = Convert.ToString(e.Tag);
 
             if (UserManager.AuthenticateUser(e.Tag))
@@ -40,14 +37,10 @@ namespace ICT4EVENT
                 rfid.Error -= RFID_Error;
                 rfid.Tag -= RFID_Tag;
                 rfid.TagLost -= rfid_TagLost;
-                //Settings.ActiveUser = UserManager.FindUserFromRFID(e.Tag);
                 TagEventHandler tag = this.btnLogin_Click;
 
                 this.Invoke(tag, new object[]{sender, e});
             }
-
-            rfid.Antenna = true;
-            rfid.LED = true;
         }
 
         private void OpenRFIDConnection()
@@ -118,6 +111,9 @@ namespace ICT4EVENT
 
             rfid.Antenna = false;
             rfid.LED = false;
+            rfid = new RFID();
+                
+            rfid.close();
 
             FillActionList();
 
