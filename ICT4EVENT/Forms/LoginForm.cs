861ad0ed8210cb7a47247dc3b9ab78ec81c8d7cc
luudvkeulen <l.vankeulen@student.fontys.nl>
2015-04-29 13:49:21 +0200

    Fix for rfid error?

--- ICT4EVENT/Forms/LoginForm.cs
+++ ICT4EVENT/Forms/LoginForm.cs
@@ -80,8 +80,8 @@ namespace ICT4EVENT
             if (regs == null && Settings.ActiveUser.Username != "admin")
             {
                 MessageBox.Show("Registeer je voor een event");
-                rfid.close();
-                form_Closed(this, new EventArgs());
+                //form_Closed(this, new EventArgs());
+                Application.Restart();
                 return false;
             }
 
